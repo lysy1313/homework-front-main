dcf0709db18b9014405663626ce480eb8f250c3e
lysy <lysy.ilya1313@gmail.com>
2025-11-07 01:29:44 +0300

    first commit

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,37 @@
+type AddressType = {
+  street: any; // ПОДПРАВЛЯЕМ any
+  city: any; // ПОДПРАВЛЯЕМ any
+};
+
+type UserType = {
+  id: number;
+  name: string;
+  age: number;
+  address: AddressType;
+};
+
+type UserListPropsType = {
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={"hw01-users"}>
+      <h2>User List:</h2>
+
+      <ul>
+        {props.users.map(
+          (user) => {
+            return (
+              <li key={user.id} id={`hw01-user-${user.id}`}>
+                <strong>{user.name}</strong> (Age: {user.age})
+                <strong> Address:</strong>
+                {user.address.street}, {user.address.city}
+              </li>
+            );
+          } // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+        )}
+      </ul>
+    </div>
+  );
+};
